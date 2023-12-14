52652ca9a765c009393b13c37927c8e3a41f5d8e
bufferum7011 <bufferum@yandex.ru>
2023-12-14 15:26:42 +0300

    todays_date

--- clean.bat
+++ clean.bat
@@ -3,7 +3,11 @@ timeout 5
 mvn clean -f "d:\_jsource\epsilon\pom.xml"
 timeout 5
 mvn compile -f "d:\_jsource\epsilon\pom.xml"
-timeout 5
+
+SET todays_date = data /T
+
 git add .
 git add .
+git commit -m todays_date
+git push
 timeout 5
