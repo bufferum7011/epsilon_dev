ec47fdd34bf9b63e3af4aafeba36b641e52d7ae2
bufferum7011 <bufferum@yandex.ru>
2024-01-13 04:29:04 +0300

    v0.45 контроль стилей 4 - поменяю типизацию на object

--- src/main/java/epsilon/Style_control.java
+++ src/main/java/epsilon/Style_control.java
@@ -1,23 +1,21 @@
 package epsilon;
 import static epsilon.Panel.print;
+
 import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
 
-import javafx.collections.ObservableList;
-import javafx.scene.Node;
-import javafx.scene.layout.Pane;
+import javafx.scene.Parent;
 import javafx.stage.Stage;
-import javafx.util.Pair;
 
-public class Style_control<T, String> extends Pane {
+public class Style_control<T, String> {
 
     private Stage stage;
     private T t;
     private String style_name;
     private List<Style_control<T, String>> list = new ArrayList<>();
 
-    private Style_control() {}
+    // private Style_control() {}
     public Style_control(Stage stage) {
         this.stage = stage;
     }
@@ -25,46 +23,92 @@ public class Style_control<T, String> extends Pane {
         this.t = t;
         this.style_name = style_name;
     }
-    
-    public void add(T t, String style_name) {
-        print.debag("1");
-        list.add(new Style_control<T, String>(t, style_name));
-        print.debag("2");
-        // foreach();
-    }
-    public void remove(String style_name) {
 
-        // for(int i = 0; list.iterator().hasNext(); i++) {
-        //     if(!style_name.equals(list.iterator().next())) {
-        //         list.remove(i);
-        //         update();
-        //         break;
-        //     }
-        // }
+    public void add(T t, String style_name) {
+
+        // Задача: добавить новый стиль для объекта
+
+        // Создание итератора
+        Iterator<Style_control<T, String>> iterator = list.iterator();
+
+        boolean key = true;
+        while(key && iterator.hasNext()) {
+
+            // Получение текущего элемента из уже существующего list
+            Style_control<T, String> next = iterator.next();
+
+            // Проверка на дубликат
+            if(next.t == t) {
+                key = false;
+                print.error("ЭЛЕМЕНТ УЖЕ СУЩЕСТВУЕТ\n");
+            }
+        }
+
+        if(key) {
+            list.add(new Style_control<T, String>(t, style_name));
+            update();
+        }
+
     }
     private void update() {
 
-        stage.getScene().getStylesheets().clear();
+        // Задача: удалить стили и заполнить заново используя их обновленный список
 
-        // while(list.iterator().hasNext()) {
-        //     stage.getScene().getStylesheets().add(list.iterator().next());
-        // }
+        // Создание итератора
+        Iterator<Style_control<T, String>> iterator = list.iterator();
+
+        // Очистка стилей
+        for(int i = 0; iterator.hasNext(); i++) {
+
+            try {
+                iterator.next();
+                print.debag(i + "\t");
+                stage.getScene().getStylesheets().remove(i);
+                print.debag(i + "\t");
+            }
+            catch(Exception e) {
+                print.error(i + "\t");
+            }
+        }
+
+        // Перезаполнение стилей
+        iterator = list.iterator();
+
+        print.debag("\nПерезаполнение стилей\n");
+        for(int i = 0; i < list.size(); i++) {
+            
+            // Получение текущего элемента из уже существующего list
+            Style_control<T, String> next = iterator.next();
+            print.debag(i + "\t");
+            try {
+                ((Parent) next.t).getStylesheets().add((java.lang.String) next.style_name);
+            }
+            catch (Exception e) {
+                print.error("Перезаполнение стилей\n");
+            }
+
+        }
+
+        boolean key = true;
+        if(key) {
+            list.add(new Style_control<T, String>(t, style_name));
+            update();
+        }
+    }
+    public void remove(String style_name) {
+
     }
     public void foreach() {
 
         print.debag("=====foreach=\n");
         Iterator<Style_control<T, String>> iterator = list.iterator();
-        while(iterator.hasNext()) {
-            Style_control<T, String> i = iterator.next();
-            print.way(i.t + "\n");
-            print.way(i.style_name + "\n");
-            print.way(i.getId() + "\n");
+        for(int i = 0; iterator.hasNext(); i++) {
+
+            Style_control<T, String> next = iterator.next();
+            print.way(next.t + "\n");
+            print.way(next.style_name + "\n");
+            print.way(i + "\n");
         }
-    }
-    @Override public ObservableList<Node> getChildren() {
-        ObservableList<Node> l = super.getChildren();
-        // l.
-        return super.getChildren();
     }
 
 }
